b86d47d61697a704be42e4de64b6d03009bac8f4
smc <crazysmc@users.noreply.github.com>
2023-11-21 23:16:41 +0100

    initial mod version

---
+++ README.md
@@ -0,0 +1,9 @@
+# No Main Hand
+
+A simple client-side fabric mod that visually removes the main hand.
+The hand in first person as well as the player model main arm are set to be
+invisible.
+
+Changing the main hand game option decides which arm is hidden.
+The sleeve of the skin can be left enabled or disabled via its vanilla
+setting.
---
+++ src/client/java/io/github/crazysmc/mixin/client/GameRendererMixin.java
@@ -0,0 +1,21 @@
+package io.github.crazysmc.mixin.client;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin
+{
+  @Shadow
+  public abstract void setRenderHand(boolean renderHand);
+
+  @Inject(at = @At("HEAD"), method = "renderWorld")
+  public void renderWorld(CallbackInfo ci)
+  {
+    setRenderHand(false);
+  }
+}
